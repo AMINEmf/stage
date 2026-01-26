9d6a5c7e480d7165a8a85853f654ce7fd38a81bc
MounibLex <mouniblex@gmail.com>
2026-01-26 11:26:22 +0100

    Convert submodules to regular directories and track all files

---
+++ Projet2/ProjetFront/src/App.jsx
@@ -0,0 +1,62 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
+import Navigation from './Acceuil/Navigation';
+
+import Dashboard from './Acceuil/Dashboard';
+import DepartementManager from './Zakaria/Employe/DepartementManager';
+
+
+import EmpHistorique from './Zakaria/EmpHistorique.jsx';
+
+
+import { OpenProvider } from './Acceuil/OpenProvider.jsx';
+
+
+
+
+
+// HeaderProvider import for global header state
+import { HeaderProvider } from './Acceuil/HeaderContext';
+import Societe from './Zakaria/Societe/Societe.jsx';
+import Login from './Login/Login.jsx';
+
+const App = () => {
+  const location = useLocation();
+  const showNavigation = location.pathname !== '/login';
+  return (
+    <AuthProvider>
+      <OpenProvider>
+        <HeaderProvider>
+          {showNavigation && <Navigation />}
+          <Routes>
+                        <Route path="/" element={<Dashboard />} />
+
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/employes" element={<DepartementManager />} />
+
+
+
+            <Route path="/emphistorique" element={<EmpHistorique />} />
+
+
+
+
+
+       
+            <Route path="/societes" element={<Societe/>}/>
+
+
+
+
+          </Routes>
+        </HeaderProvider>
+      </OpenProvider>
+    </AuthProvider>
+  );
+};
+
+export default App;
